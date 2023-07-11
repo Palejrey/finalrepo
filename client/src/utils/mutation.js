702a620b1af370e32354d2c28319b361b4132df3
palejrey@gmail.com <palejrey@gmail.com>
2023-07-10 20:02:18 -0500

    deploying

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -10,7 +10,7 @@ mutation createUser($name:String!,  $password:String!, $email:String!) {
     }   
 }`;
 export const LOGIN_USER=gql`
-mutation login($email:string!, $password:string!){
+mutation login($email:String!, $password:String!){
     login(email:$email, password:$password){
         token
         profile{
@@ -21,7 +21,7 @@ mutation login($email:string!, $password:string!){
  }
 `;
 export const CREATE_JOB=gql`
-mutation createJob($title:string!, $description:string!){
+mutation createJob($title:String!, $description:String!){
     createJob(title:$title, description:$description){
         _id
         title
